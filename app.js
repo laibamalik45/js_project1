5c7346a3cceec05e8d1b4778749b22ace6ff4a35
laibamalik45 <laibamalik45d@gmail.com>
2023-09-07 23:15:00 +0500

    now ready to commit

---
+++ app.js
@@ -0,0 +1,112 @@
+// CHAPTER NO 1
+
+// Q no 1
+// alert("laiba");
+// alert("malik");
+// alert("laibamalik45d@gmail.com");
+// alert("03242746348");
+// alert("123");
+
+// Q no 2
+// alert("You're learning JavaScript!");
+
+// q no 3
+// alert("learning javascript!");
+
+
+//  chapter no 2
+
+// q no 1
+// var firstName = "laiba";
+// console.log(firstName);
+
+// q no 2
+// var num;
+// num = 123;
+// console.log(num);
+
+// q no 3
+// var teamName;
+// alert("pakistan");
+
+// q no 4
+// var bestMan = "hammad";
+// console.log(bestMan);
+
+// CHAPTER NO 3
+
+//  q no 1
+// var firstName;
+
+//  q no 2
+// var caseQty = 144;
+// document.write(caseQty);
+
+//  q no 3
+// var num = 9;
+
+// q no 4
+// var num;
+// num = 2 + 2;
+// console.log(num);
+
+// q no 5
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// console.log(orderTotal);
+
+// q no 6
+// var num1 = 3;
+// var num2 = 9;
+// console.log(num1+num2);
+
+//  CHAPTER NO 4
+
+// q no 1
+// var product_cost = 3.45;
+
+// q no 2
+// var nameOfband;
+
+// q no 3
+// var isStudent = 4;
+
+// q no 4
+// var firstName = "laiba";
+// var lastName = "mahjabeen";
+// alert(firstName+lastName);
+
+// q no 5
+//  legal
+//  name,$ali,firstName,lastName,Max_count
+//  illegal
+//  123laiba,2nd variable,laiba $ 06
+
+// CHAPTER NO 5
+
+//  q no 1
+// mudolus %
+
+// q no 2
+// var num = 20 % 6;
+// console.log(num);
+
+// q no 3
+// var largeNumb = 1000*2000;
+// console.log(largeNumb);
+
+// q no 4
+// var num = 7;
+// var res = num-3;
+// console.log(res);
+
+// q no 5
+//  var num = 45;
+//  var res = num % 30;
+//  console.log(res);
+
+//  q no 6
+// alert(3*8);
+
+// end
